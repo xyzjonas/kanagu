9dc4fac136f93fec809a44b51437f3b40e97b2eb
jonas <jonasbrauer@seznam.cz>
2025-06-09 12:35:39 +0200

    setup Sentry

--- Justfile
+++ Justfile
@@ -1,6 +1,6 @@
 [working-directory: 'ansible']
 deploy:
-    ansible-playbook deploy-playbook.yml -i inventory
+    VITE_SENTRY_ENV=production ansible-playbook deploy-playbook.yml -i inventory
 
 dev:
     npm run dev
@@ -13,9 +13,9 @@ openapi-gen:
     npm run openapi-ts
 
 docker-build:
-    docker build -t scotch3840/kanagu .
+    VITE_SENTRY_ENV=production docker build -t scotch3840/kanagu .
 
 docker:
-    docker build -t scotch3840/kanagu .
+    VITE_SENTRY_ENV=production docker build -t scotch3840/kanagu .
     docker push scotch3840/kanagu
 
--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,28 @@ import router from './router'
 
 import 'virtual:uno.css'
 
+import * as Sentry from "@sentry/vue";
+
 const app = createApp(App)
+
+
+const sentryDsn = import.meta.env.VITE_SENTRY_DSN
+const sentryEnv = import.meta.env.VITE_SENTRY_ENV
+if (sentryDsn) {
+  Sentry.init({
+    app,
+    dsn: sentryDsn,
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: false,
+    integrations: [
+      Sentry.browserTracingIntegration(),
+    ],
+    tracesSampleRate: 1.0,
+    environment: sentryEnv ?? 'dev'
+  });
+  console.info(`Sentry initialized [${sentryEnv}]: ${sentryDsn}`)
+}
 
 app.use(Quasar, {
   plugins: {
